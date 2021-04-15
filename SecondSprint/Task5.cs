b9c93969e421aa66bec7ae5f633b2aba63ab25ef
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2021-04-15 16:12:26 +0300

    Add files via upload

---
+++ SecondSprint/Task5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSpring
+{
+    class Acinonychini
+    {
+
+    }
+
+    sealed class Acinonyx : Acinonychini
+    {
+
+    }
+    class Puma : Acinonychini
+    {
+
+    }
+}
