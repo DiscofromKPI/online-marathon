adafd2711c00ab4bf347e42f52ea39c1936a8301
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2021-04-19 14:26:22 +0300

    Add files via upload

---
+++ ThirdSprint/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSpring
+{
+    public delegate int CalcDelegate(int f, int s, char o);
+    class CalcProgram
+    {
+        public static int Calc(int first, int second, char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return first + second;
+                case '-':
+                    return first - second;
+                case '*':
+                    return first * second;
+                case '/':
+                    if (second == 0) return 0;
+                    else return first / second;
+            }
+            return 0;
+        }
+        CalcDelegate funcCalc = Calc;
+    }
+}
---
+++ ThirdSprint/Task4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSpring
+{
+    public static class IListExtensions
+    {
+        public static List<int> IncreaseWith(this List<int> a, int value)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                a[i] += value;
+            }
+            return a;
+        }
+    }
+    public static class IEnumerableExtensions
+    {
+        public static string ToString<T>(this List<int> a)
+        {
+            return '[' + string.Join(", ", a.ToArray()) + ']';
+        }
+    }
+}
