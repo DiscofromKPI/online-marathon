63268fd75cb46dd3d161630a841052a129bc7045
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2021-05-10 15:57:38 +0300

    Update Task2.cs

--- FourthSprint/Task2.cs
+++ FourthSprint/Task2.cs
@@ -6,35 +6,35 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 namespace FirstSpring
 {
-    //public interface IAnimal
-    //{
-    //    string Name { get; set; }
-    //    void Voice();
-    //    void Feed();
-    //}
-    //class Cat : IAnimal
-    //{
-    //    public string Name { get; set; }
-    //    public void Voice()
-    //    {
-    //        Console.WriteLine("Mew");
-    //    }
-    //    public void Feed()
-    //    {
-    //        Console.WriteLine("I eat mice");
-    //    }
-    //}
-    //class Dog : IAnimal
-    //{
-    //    public string Name { get; set; }
-    //    public void Voice()
-    //    {
-    //        Console.WriteLine("Woof");
-    //    }
-    //    public void Feed()
-    //    {
-    //        Console.WriteLine("I eat meat");
-    //    }
-    //}
+    public interface IAnimal
+    {
+        string Name { get; set; }
+        void Voice();
+        void Feed();
+    }
+    class Cat : IAnimal
+    {
+        public string Name { get; set; }
+        public void Voice()
+        {
+            Console.WriteLine("Mew");
+        }
+        public void Feed()
+        {
+            Console.WriteLine("I eat mice");
+        }
+    }
+    class Dog : IAnimal
+    {
+        public string Name { get; set; }
+        public void Voice()
+        {
+            Console.WriteLine("Woof");
+        }
+        public void Feed()
+        {
+            Console.WriteLine("I eat meat");
+        }
+    }
 
 }
