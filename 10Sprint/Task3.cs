14063dc27970b61e65a881e1672ade21627e3532
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2021-05-13 14:41:30 +0300

    Create Task3.cs

---
+++ 10Sprint/Task3.cs
@@ -0,0 +1,72 @@
+interface INotification
+    {
+        void SendNotification();
+    }
+    interface INotificationToDB
+    {
+        void AddNotificationToDB();
+    }
+    
+    public class MailService : INotification, INotificationToDB
+    {
+        string Email { get; set; }
+
+        string EmailTitle { get; set; }
+        string EmailBody { get; set; }
+        public MailService(string email, string emailbody, string emailtitle)
+        {
+            Email = email;
+            EmailTitle = emailtitle;
+            EmailBody = emailbody;
+        }
+        public void SendNotification()
+        {
+            Console.WriteLine($"Mail:{Email}, Title:{EmailTitle}, Body:{EmailBody}");
+        }
+        public bool ValidEmail()
+        {
+            return Email.Contains("@");
+        }
+
+        public void AddNotificationToDB()
+        {
+           
+        }
+    }
+    public class SmsService : INotification
+    {
+        string Number { get; set; }
+
+        string SmsText { get; set; }
+        public SmsService(string number, string smsText)
+        {
+            Number = number;
+            SmsText = smsText;
+        }
+        public void SendNotification()
+        {
+            Console.WriteLine($"Number:{Number}, Message:{SmsText}");
+        }
+    }
+    public class Customer
+    {
+        public static void Register(string email, string password)
+        {
+            MailService ms = new MailService(email, "Body of message...", "User registration");
+            SmsService ss = new SmsService("111 111 111", "User successfully registered...");
+            try
+            {
+                if (ms.ValidEmail())
+                {
+                    ms.SendNotification();
+                    ms.AddNotificationToDB();
+                    ss.SendNotification();
+                    
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
